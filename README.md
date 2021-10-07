9c7ffb088062f998136438742459066837373b2a
Jon Fuller <Zibilianja@users.noreply.github.com>
2021-10-07 14:42:43 -0600

    Build tests for cypress QA assignment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # QA-LIbrary
-A cloned repo containing my library project for developing QA automated tests
+
+A cloned repo containing my library project for developing QA automated tests;
+##Instructions
+
+1. Clone this repository locally.
+
+2. Navigate to the project folder and run "npm install" to install project and it's dependencies.
+
+3. In terminal, run "npx json-server db.json".
+
+4. Open a new terminal tab, run "parcel index.html", ensure that it is running at localhost:1234 for testing congruency
+
+5. Open a new terminal tab, run "npm test" to run tests in Cypress.
---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,75 @@
+// describe('1_validate_bookshelf_links', () => {
+//     it('Home_Header_Nav_Link', () => {
+//         cy.visit('http://localhost:1234');
+//         cy.get('.header--bookshelf').click();
+//         cy.url().should('include', 'bookshelf');
+//     });
+//     it('Home_Main_Button', () => {
+//         cy.visit('http://localhost:1234');
+//         cy.contains('See Books').click();
+//         cy.url().should('include', 'bookshelf');
+//     });
+//     it('AddBook_Nav_Link', () => {
+//         cy.visit('http://localhost:1234/addbook');
+//         cy.get('.header--bookshelf').click();
+//         cy.url().should('include', 'bookshelf');
+//     });
+// })
+
+const { iteratee } = require("lodash")
+
+// describe('2_validate_Title_Link_To_Home', () => {
+//     it('Bookshelf__Click__Title__Link', () => {
+//         cy.visit('http://localhost:1234/bookshelf');
+//         cy.get('.header__title').click();
+//         cy.url().should('eq', 'http://localhost:1234/')
+//     })
+// })
+
+// describe('3_validate_delete_book', () => {
+//     it('Book_check_exists', () => {
+//         cy.visit('http://localhost:1234/bookdetails/123');
+//         cy.wait(1500).get('.book__title').contains('Stone')
+//     });
+//     it('Book_delete_redirect_to_bookshelf', () => {
+//         cy.get('.delete__button').click();
+//         cy.url().should('include', 'bookshelf')
+//     });
+//     it('Check_page_for_deleted_book', () => {
+//         cy.wait(1500).get('.grid__title').contains('Stone').should('not.exist')
+//     })
+// });
+
+describe('4_validate_addbook_link', () => {
+    it('Redirect_home_to_addbook', () => {
+        cy.visit('http://localhost:1234/');
+        cy.get('.header--addbook').click();
+        cy.url().should('include', 'addbook');
+    });
+    it('Redirect_bookshelf_to_addbook', () => {
+        cy.visit('http://localhost:1234/bookshelf');
+        cy.get('.header--addbook').click();
+        cy.url().should('include', 'addbook');
+    });
+    it('Redirect_edit_to_addbook', () => {
+        cy.visit('http://localhost:1234/editbook');
+        cy.get('.header--addbook').click();
+        cy.url().should('include', 'addbook');
+    });
+});
+
+describe('5_validate_book_details', () => {
+    it('Navigate_to_bookshelf', () => {
+        cy.visit('http://localhost:1234/bookshelf');
+        cy.url().should('include', 'bookshelf');
+    });
+    it('Click_on_book_1', () => {
+        cy.wait(1000).get('.grid__title').contains('Chamber').click();
+        cy.url().should('include', 'details')
+    });
+    it('Check_book', () => {
+        cy.wait(1000).get('.book__title').contains('Chamber');
+
+    });
+});
+
